90cdc41b9dcde7f21617d1208caedf11685a9350
Qwwerty <verdinhoraro2009@hotmail.com>
2021-02-22 21:46:59 -0300

    Tela de cadastro

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  FogoutPassword,
+  ForgoutPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+        <Container>
+          <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
+          <Title>Faça seu logon</Title>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button onPress={() => {}}>Entrar</Button>
-    </Container>
+          <Button onPress={() => {}}>Entrar</Button>
+
+          <FogoutPassword onPress={() => {}}>
+            <ForgoutPasswordText>Esqueci minha senha</ForgoutPasswordText>
+          </FogoutPassword>
+        </Container>
+      </ScrollView>
+
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
